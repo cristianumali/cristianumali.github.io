38bde9724d3a5e0ef4e34faf054ebfb3fc86dcf6
Cristian Umali <cristian.umali@templeandwebster.com.au>
2022-11-11 18:54:26 +0800

    :tada: Added typedjs animation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-  function init() {
+  let init = () => {
     initDarkModeWidget();
+    initComposerTyping();
   }
-  function initDarkModeWidget() {
+  
+  let initDarkModeWidget = () => {
     const options = {
       time: '0.5s', // default: '0.3s'
       mixColor: '#fff', // default: '#fff'
@@ -13,8 +15,40 @@ $(document).ready(function () {
       label: '🌓', // default: ''
       autoMatchOsTheme: true // default: true
     }
-    new Darkmode(options).showWidget();
+    
+    const darkMode = new Darkmode(options);
+    darkMode.showWidget()
   }
   
+  let initTypingAction = () => {
+    let options = {
+      strings: ['Web Developer', 'Full Stack Engineer'],
+      typeSpeed: 40,
+      smartBackspace: true,
+      startDelay: 100,
+      backSpeed: 50,
+      loop: true
+    };
+  
+    new Typed('.type-field', options);
+  }
+  
+  let initComposerTyping = () => {
+    new Typed('.install-animate', {
+      strings: ['$ ^500 npm install^1000 `<br>installing components ...` ^1000\n `<br>Fetching from source ...` ^500'],
+      typeSpeed: 40,
+      backSpeed: 0,
+      onComplete: () => {
+        $('.intro').addClass('hidden');
+        $('.main-text').css({
+          'visibility': 'visible',
+          'opacity': 1
+        });
+        initTypingAction();
+      }
+    });
+  
+  }
+
   init();
 })
